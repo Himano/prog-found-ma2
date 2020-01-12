b26ee74028e617f144eec62c14bfbe9757b55d5d
Himano <48498343+Himano@users.noreply.github.com>
2020-01-12 12:44:40 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+//Question 1
+var petArray = ["cat", "cow", "dog"];
+
+//Question 2
+console.log(petArray[1]);
+
+//Question 3
+petArray.push("sheep");
+
+//Question 4
+console.log(petArray.length);
+
+//Question 5
+var catObject = {
+  name: "Mittens",
+  color: "Brown",
+  age: 21
+};
+
+//Question 6
+console.log(catObject.age);
+
+//Question 7
+var catArray = [
+  {
+    name: "Mittens",
+    color: "Brown",
+    age: 21
+  }
+];
+
+//Question 8
+for (var i = 0; i < catArray.length; i++) {
+  console.log(catArray[i]);
+}
+
+//Question 9
+function logtoConsole(text) {
+  console.log(text);
+}
+
+//Question 10
+logtoConsole("donkey");
